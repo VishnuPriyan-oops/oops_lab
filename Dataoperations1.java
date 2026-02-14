859893aadae55e10ccd80c4f1ab3ef2d6e3ed3b8
VishnuPriyan-oops <vishnuviruthagiri@gmail.com>
2026-02-14 21:27:54 +0530

    Add files via upload

---
+++ Dataoperations1.java
@@ -0,0 +1,10 @@
+class Dataoperations1{
+	public static void main(String args[]){
+		int a=64;
+		int b=32;
+		System.out.println("Addition of Two Numbers:"+(a+b));
+		System.out.println("Subtraction of Two Numbers:"+(a-b));
+		System.out.println("Multiplication of Two Numbers:"+(a*b));
+		System.out.println("Division of Two Numbers:"+(a/b));
+		}
+}
---
+++ InputOutput1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class InputOutput1{
+	public static void main(String args[]){
+		Scanner rad=new Scanner(System.in);
+		float r=rad.nextFloat();
+		System.out.println("The area of circle is"+(3.14*r*r));
+}
+}
