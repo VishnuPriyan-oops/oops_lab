a4ac2119798a8ef4d990735fb5617de814803fca
VishnuPriyan-oops <vishnuviruthagiri@gmail.com>
2026-02-16 12:01:56 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,9 @@
+class Forloop{
+	public static void main(String args[]){
+		int i;
+		System.out.println("Square values for numbers from 1 to 10");
+		for(i=1;i<=10;i++){
+			System.out.println(i*i);
+		}		
+	}
+}
